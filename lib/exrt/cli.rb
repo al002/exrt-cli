ee248de6ffcc80ceced522653b2e7ad1eeeb1094
al002 <al002@disroot.org>
2019-02-27 21:22:08 +0800

    init, add command

---
+++ lib/exrt/cli.rb
@@ -0,0 +1,35 @@
+require "exrt/cli/version"
+require "thor"
+require "exrt"
+
+module Exrt
+  module Cli
+    class CLI < Thor
+      class_option :base, :type => :string, :default => "USD"
+      class_option :symbols, :type => :array, :default => []
+
+      desc "latest --base --symbols", "show latest exchange rates"
+      def latest
+        base = options["base"]
+        symbols = options["symbols"]
+        response = Exrt::Rate.latest(base: base, symbols: symbols)
+        puts response
+      end
+
+      desc "history --base --symbols --start_at --end_at", "show history exchange rates"
+      option :start_at, :type => :string, :required => true
+      option :end_at, :type => :string, :required => true
+      def history
+        base = options["base"]
+        symbols = options["symbols"]
+        response = Exrt::Rate.history(
+          base: base,
+          symbols: symbols,
+          start_at: options["start_at"],
+          end_at: options["end_at"]
+        )
+        puts response
+      end
+    end
+  end
+end
---
+++ spec/exrt/cli_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Exrt::Cli do
+  it "has a version number" do
+    expect(Exrt::Cli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
